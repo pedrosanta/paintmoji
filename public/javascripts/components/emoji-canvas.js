d714a6f7b2c2936278bef9b7fa4b36667ade9d05
Pedro Machado Santa <pedro.santa@gmail.com>
2024-04-15 02:48:59 +0100

    Add example click event

--- public/javascripts/components/emoji-canvas.js
+++ public/javascripts/components/emoji-canvas.js
@@ -12,11 +12,22 @@ class EmojiCanvas extends HTMLElement {
     this.appendChild(this.cursorEl);
 
     this.addEventListener('mousemove', this.handleMouseMove.bind(this));
+    this.addEventListener('click', this.handleClick.bind(this));
   }
 
   handleMouseMove(event) {
     this.cursorEl.style.top = event.y - this.offsetTop;
     this.cursorEl.style.left = event.x - this.offsetLeft;
+  }
+
+  handleClick(event) {
+    const x = event.x - this.offsetLeft;
+    const y = event.y - this.offsetTop;
+
+    const top = y * 100 / this.offsetHeight;
+    const left = x * 100 / this.offsetWidth;
+
+    console.log('Emoji canvas click:', top, left);
   }
 }
 
